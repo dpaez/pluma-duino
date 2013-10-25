46e38aaa38f4d5637fb3fdd8d6d708a157148fd0
Diego Paez <paez.di@gmail.com>
2013-10-25 10:05:46 -0300

    WIP new components modules

---
+++ components/comp_lcd.js
@@ -0,0 +1,8 @@
+module.exports = exports = CompLCD;
+
+function CompLCD( options ){
+  // create J5 LCD module here _inside_
+  
+  // check: https://github.com/visionmedia/express/blob/master/lib/view.js
+}
+
